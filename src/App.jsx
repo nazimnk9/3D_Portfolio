2131200509b04ea6911176b2d41f22f3ccb084ce
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2025-05-03 17:47:25 +0600

    add 3d model image and counter up

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,7 @@ import Hero from "./sections/Hero"
 
 const App = () => {
   return (
-    <section id="hero" className="relative overflow-hidden">
-      <div className="absolute top-0 left-0 z-10">
-        <img src="/images/bg.png" alt="background" />
-      </div>
-      <div className="hero-layout">
-        {/* LEFT HERO CONTENT */}
-        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-          <div className="flex flex-col gap-7">
-            <div className="hero-text">
-              <h1 className="">Shaping</h1>
-              <h1 className="">into Real Projects</h1>
-              <h1 className="">that Deliver Results</h1>
-            </div>
-          </div>
-        </header>
-        {/* RIGHT 3D MODEL */}
-      </div>
-    </section>
+    <><Hero /></>
   )
 }
 
