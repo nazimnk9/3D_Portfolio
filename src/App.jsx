1f884c3498aa2f0939d65353c2965980910f7110
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2025-05-04 10:37:43 +0600

    add showcase project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
-    <><Hero /></>
+    <>
+      <Hero />
+      <ShowcaseSection />
+    </>
   )
 }
 
