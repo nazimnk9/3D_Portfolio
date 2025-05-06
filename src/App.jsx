b813778fb48eb06520530108037972e523181645
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2025-05-06 12:15:50 +0600

    add navbar, feature card, logo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import LogoSection from "./sections/LogoSection"
+import NavBar from "./components/NavBar"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
     <>
+    <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
     </>
   )
 }
